747eb0c1e6c3e431cc03d108bd1b3dbee62002c6
527271257 <527271257@qq.com>
2016-09-23 15:49:10 -0400

    add

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 from flask import Flask
 from redis import Redis
 
-
-
-
-app=Flask(_name_)
-
-redis=Redis(host='redis',port=6379)
-
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
-defhello():
-redis.incr('hits')
-    return'Hello World ! this is %s times.' %redis.get('hits')
+def hello():
+    redis.incr('hits')
 
-if _name_=="_main_":
-   app.run(host="0.0.0.0",debug=True)
+    return 'Hello World! I have been seen %s times.'  % redis.get('hits')
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
