9db682ec955ac82cd0c9624518d435068b6cc549
527271257 <527271257@qq.com>
2016-09-23 13:09:12 -0400

    edit test-build

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -5,5 +5,5 @@ docker push 192.168.5.34:80/python-redis-demo:${BUILD_NUMBER}
 cd ${WORKSPACE}/test-build
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
-chmod 777 ./rancher-compose
-./rancher-compose --access-key 7798EBA1969968CA8C3E  --secret-key Nhb2cFM7USih8Ssu7TosyH9izzWQ686diki17wDm -p python-redis-demo-build${BUILD_NUMBER} up -d
+chmod 777 ./rancher-compose.yml
+./rancher-compose.yml --access-key 7798EBA1969968CA8C3E  --secret-key Nhb2cFM7USih8Ssu7TosyH9izzWQ686diki17wDm -p python-redis-demo-build${BUILD_NUMBER} up -d
