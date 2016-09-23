98c21cc6c79d0bbc2e7af9a0d1f6f54097c17e8e
527271257 <527271257@qq.com>
2016-09-23 15:42:50 -0400

    add

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -6,4 +6,4 @@ cd ${WORKSPACE}/test-build
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
-./rancher-compose --access-key 7798EBA1969968CA8C3E  --secret-key Nhb2cFM7USih8Ssu7TosyH9izzWQ686diki17wDm -p python-redis-demo-build${BUILD_NUMBER} up -d
+./rancher-compose  --access-key 7798EBA1969968CA8C3E  --secret-key Nhb2cFM7USih8Ssu7TosyH9izzWQ686diki17wDm -p python-redis-demo-build${BUILD_NUMBER} up -d
